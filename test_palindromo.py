7caaa5159fab9d72fa1ffdf655eea8eb847911f6
Juan Pablo Perea <jppereah@correo.udistrital.edu.co>
2022-09-24 07:37:52 -0500

    Unit Test

--- test_palindromo.py
+++ test_palindromo.py
@@ -1,4 +1,5 @@
 from palindromo import palindromo, subPalindromo
+import pytest
 
 
 def test_palindromo():
@@ -7,3 +8,16 @@ def test_palindromo():
 
 def test_subPalindromo():
     assert subPalindromo('AABBsopAsAposCCDD') == 'sopAsApos'
+
+
+@pytest.mark.parametrize(
+    "input_a, expected",
+    [
+        ('Anita lava la tina', True),
+        ('AABBsopAsAposCCDD', False),
+        ('123456 6 54321', True)
+
+    ]
+)
+def test_palindromo_multi(input_a, expected):
+    assert palindromo(input_a) == expected
